b323f5c939b820e0210abadf9070d68f8e351866
alex-nk89 <nagaytsevae@nknh.sibur.ru>
2023-11-23 20:40:50 +0300

    feat: pages padding wrapper refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import { AppDecorator } from "./components/organisms/Layout";
 import { MainView, MovieView } from "./components/pages";
+import { Suspense, lazy } from "react";
+
+const GalleryView = lazy(
+  () => import("../src/components/pages/GalleryView//GalleryView")
+);
 
 function App() {
   return (
     <AppDecorator>
       <Routes>
         <Route path="" element={<MainView />} />
-        <Route path="/movie/:id" element={<MovieView />} />
+        <Route path="/movie">
+          <Route path=":id">
+            <Route path="" element={<MovieView />} />
+            <Route
+              path="images"
+              element={
+                <Suspense fallback={<div>Loading...</div>}>
+                  <GalleryView />
+                </Suspense>
+              }
+            />
+          </Route>
+        </Route>
       </Routes>
     </AppDecorator>
   );
